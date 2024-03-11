0dc0f8e94d7ff080b16a00b4d9dc87fead914b88
Admin <Admin@DESKTOP-IEPVL3O.bbrouter>
2024-03-11 12:23:07 +0530

    added homepage class

--- com.sevenrmartsupermarket/src/main/java/com/sevenrmartsupermarket/pages/HomePage.java
+++ com.sevenrmartsupermarket/src/main/java/com/sevenrmartsupermarket/pages/HomePage.java
@@ -1,5 +1,7 @@
 package com.sevenrmartsupermarket.pages;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
@@ -7,11 +9,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import com.sevenrmartsupermarket.utilities.GeneralUtility;
+
 public class HomePage {
 	
 		WebDriver driver;
 		 @FindBy(xpath="//a[@class='d-block']")
 		 WebElement profileName;
+		 @FindBy(xpath = "//div[@class='inner']")
+			List<WebElement> listOfContent;
+		 GeneralUtility generalutility=new GeneralUtility();
 		 public HomePage(WebDriver driver) {
 				this.driver=driver;
 				PageFactory.initElements(driver,this);
@@ -19,5 +26,11 @@ public class HomePage {
 }
 		 public String getProfileName() {
 			 return(profileName.getText());
+		 }
+		 public List<String> content(){
+			 List<String> contentList=new ArrayList();
+			 contentList=generalutility.getTextofElements(listOfContent);
+			 return contentList;
+			 
 		 }
 }
