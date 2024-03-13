e537f9838ff8c487b338150c33ec2efa02a300a2
Admin <Admin@DESKTOP-IEPVL3O.bbrouter>
2024-03-13 20:29:07 +0530

    name changed

--- com.sevenrmartsupermarket/src/test/java/com/sevenrmartsupermarket/tests/LoginTest.java
+++ com.sevenrmartsupermarket/src/test/java/com/sevenrmartsupermarket/tests/LoginTest.java
@@ -9,43 +9,46 @@ import com.sevenrmartsupermarket.pages.LoginPage;
 import com.sevenrmartsupermarket.utilities.ScreenShot;
 
 public class LoginTest extends Base {
- 
+
 	HomePage homepage;
-	 LoginPage loginpage;
-	 
-	
+	LoginPage loginpage;
+
 	@Test(retryAnalyzer = com.sevenrmartsupermarket.listeners.RetryAnalyzer.class)
 	public void verifyLogin() {
-		loginpage=new LoginPage(driver);
-		homepage=new HomePage(driver);
+		loginpage = new LoginPage(driver);
+		homepage = new HomePage(driver);
 		loginpage.login();
-		String expectedProfileName="Admi";
-		String actualProfileName= homepage.getProfileName();
-		Assert.assertEquals(actualProfileName,expectedProfileName);
+		String expectedProfileName = "Admin";
+		String actualProfileName = homepage.getProfileName();
+		Assert.assertEquals(actualProfileName, expectedProfileName);
 	}
-	@Test(groups="smoke")
 
-public void verifyInvalidLogin() {
-	loginpage=new LoginPage(driver);
+	@Test(groups = "smoke")
 
-	String actualInvalidMessage=loginpage.invalidlogin("abc","ert");
-	String expectedInvalidMessage="Alert!";
-			
-	
-	Assert.assertEquals(actualInvalidMessage,expectedInvalidMessage);
-	
-}@Test(groups="sanity")
-public void verifyRememberme() {
-	loginpage=new LoginPage(driver);
-	boolean x=loginpage.remembermeCheckbox();
-	
-	Assert.assertTrue(x);
-}
-@Test
-public void verifyCheckbox() {
-	loginpage=new LoginPage(driver);
-	System.out.println(loginpage.rememberme());
-}
+	public void verifyInvalidLogin() {
+		loginpage = new LoginPage(driver);
 
+		String actualInvalidMessage = loginpage.invalidlogin("abc", "ert");
+		String expectedInvalidMessage = "Alert!";
+
+		Assert.assertEquals(actualInvalidMessage, expectedInvalidMessage);
+
+	}
+
+	@Test(groups = "sanity")
+	public void verifyRememberme() {
+		loginpage = new LoginPage(driver);
+		boolean x = loginpage.remembermeCheckbox();
+
+		Assert.assertTrue(x);
+	}
+
+	@Test
+	
+	public void verifyCheckbox() {
+		loginpage = new LoginPage(driver);
+		boolean x=loginpage.rememberMe();
+		Assert.assertTrue(x);
+	}
 
 }
